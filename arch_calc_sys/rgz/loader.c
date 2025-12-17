a4bd08fab5dc4aaf5fafe2ba1b37533b22240809
Roman Moroz <morozdef24@gmail.com>
2025-12-17 17:32:51 +0300

    fix 2

--- arch_calc_sys/rgz/loader.c
+++ arch_calc_sys/rgz/loader.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
 #include <string.h>
-#include <sys/mman.h> // Нужен для mmap
+#include <sys/mman.h>
 #include <unistd.h>
 
-// Твой шелл-код
+// Обновленный шелл-код с NULL-терминатором
 unsigned char code[] = \
-"\x48\x31\xd2\x48\x31\xf6\x48\xbb\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x53\x48\x89\xe7\x48\x31\xc0\xb0\x3b\x0f\x05";
+"\x48\x31\xf6\x48\x31\xd2\x48\x31\xc0\x50\x48\xbb\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x53\x48\x89\xe7\xb0\x3b\x0f\x05";
 
 int main() {
     printf("Shellcode Length: %ld\n", sizeof(code) - 1);
 
-    // 1. Выделяем страницу памяти с правами PROT_EXEC | PROT_WRITE | PROT_READ
-    // MAP_ANONYMOUS означает, что память не привязана к файлу.
+    // Выделяем исполняемую память
     void *executable_mem = mmap(0, sizeof(code), 
                                 PROT_READ | PROT_WRITE | PROT_EXEC, 
                                 MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
     if (executable_mem == MAP_FAILED) {
-        perror("mmap failed");
+        perror("mmap");
         return 1;
     }
 
-    // 2. Копируем наш байт-код в эту исполняемую область
+    // Копируем код
     memcpy(executable_mem, code, sizeof(code));
 
-    // 3. Создаем указатель на функцию и направляем его на новую память
+    // Запускаем
     void (*ret)() = (void(*)())executable_mem;
-
-    // 4. Передаем управление
     ret();
 
     return 0;
