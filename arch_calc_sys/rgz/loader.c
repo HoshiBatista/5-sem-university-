9f7d90533de12995f3a1bf034d204373269dd283
Roman Moroz <morozdef24@gmail.com>
2025-12-17 17:38:29 +0300

    fix 3

--- arch_calc_sys/rgz/loader.c
+++ arch_calc_sys/rgz/loader.c
@@ -1,31 +1,16 @@
 #include <stdio.h>
-#include <string.h>
-#include <sys/mman.h>
-#include <unistd.h>
 
-// Обновленный шелл-код с NULL-терминатором
+// Глобальный массив с шелл-кодом
 unsigned char code[] = \
 "\x48\x31\xf6\x48\x31\xd2\x48\x31\xc0\x50\x48\xbb\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x53\x48\x89\xe7\xb0\x3b\x0f\x05";
 
 int main() {
     printf("Shellcode Length: %ld\n", sizeof(code) - 1);
-
-    // Выделяем исполняемую память
-    void *executable_mem = mmap(0, sizeof(code), 
-                                PROT_READ | PROT_WRITE | PROT_EXEC, 
-                                MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-
-    if (executable_mem == MAP_FAILED) {
-        perror("mmap");
-        return 1;
-    }
-
-    // Копируем код
-    memcpy(executable_mem, code, sizeof(code));
-
-    // Запускаем
-    void (*ret)() = (void(*)())executable_mem;
+    
+    // Прямая передача управления на адрес массива
+    // Мы приводим адрес массива к указателю на функцию и вызываем её
+    void (*ret)() = (void(*)())code;
     ret();
-
+    
     return 0;
 }
--- arch_calc_sys/rgz/scripts.sh
+++ arch_calc_sys/rgz/scripts.sh
@@ -11,4 +11,4 @@ objdump -d shellcode.o | grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -
 # Ожидаемый результат (примерно такой):
 # "\x48\x31\xd2\x48\x31\xf6\x48\xbb\x2f\x2f\x62\x69\x6e\x2f\x73\x68\x53\x48\x89\xe7\x48\x31\xc0\xb0\x3b\x0f\x05"
 
-gcc -fno-stack-protector -z execstack loader.c -o loader
+gcc -fno-stack-protector -z execstack -no-pie loader.c -o loader
